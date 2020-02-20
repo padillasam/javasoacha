b324a1b12ebf9476c7d16006cdb10722745a130f
srpadilla <srpadilla@misena.edu.co>
2020-02-20 17:19:36 -0500

    Primer commit projecto javasoacha

---
+++ src/soacha/EnSoacha.java
@@ -0,0 +1,18 @@
+package soacha;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Samuel Padilla
+ */
+public class EnSoacha {
+    public static void main(String[] args) {
+        System.out.println("Estoy en Soacha 20 feb 2020");
+    }
+    
+}
