b3bf0b5adc33187497e20effed0d9b64d603ef8a
srpadilla <srpadilla@misena.edu.co>
2020-02-21 09:30:14 -0500

    21 de febrero desde linux

--- src/soacha/DesdeLinux21feb2020.java
+++ src/soacha/DesdeLinux21feb2020.java
@@ -1,19 +1,17 @@
-package soacha;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package soacha;
 
 /**
  *
- * @author Samuel Padilla
+ * @author samuel
  */
-public class EnSoacha {
+public class DesdeLinux21feb2020 {
     public static void main(String[] args) {
-        System.out.println("Estoy en Soacha 20 feb 2020");
-        
+        System.out.println("Desde Linux el 21 de febrero de 2020");
     }
     
 }
